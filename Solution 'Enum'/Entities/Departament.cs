3775c0ff1a608c9f9557c095c07fea9834bb0648
Gabriel Gomes <gabrielgomesant@gmail.com>
2021-03-03 17:21:57 -0300

    Programa criado, entidade Worker recebe novos Contracts, juntamente com o nome do departamento (Departaments) e o Valor Recebido por contratos (Hour Contracts).

---
+++ Solution 'Enum'/Entities/Departament.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Solution__Enum_.Entities
+{
+    class Departament
+    {
+        public string Name{ get; set; }
+
+        public Departament(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Solution 'Enum'/Entities/Worker.cs
@@ -0,0 +1,34 @@
+﻿using Solution__Enum_.Entities.Enums;
+using System.Collections.Generic;
+
+namespace Solution__Enum_.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament departament { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>;
+
+        public Worker(){}
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Departament departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            this.departament = departament;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+    }
+}
